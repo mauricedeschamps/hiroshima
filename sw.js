d03f063cc2322aea565b5611ee131ffda489b315
mauricedeschamps <mementomorikatsu@gmail.com>
2026-03-30 15:24:56 +0900

    sw.js の更新

--- sw.js
+++ sw.js
@@ -1,11 +1,11 @@
 const CACHE_NAME = 'hiroshima-pwa-v1';
 const urlsToCache = [
   './',
-  './index.html',
-  './manifest.json',
+  '.index.html',
+  '.manifest.json',
   // アイコンはあなたの環境に合わせてパスを変更してください
-  // './icons/icon-192x192.png',
-  // './icons/icon-512x512.png'
+  // '.icons/icon-192x192.png',
+  // '.icons/icon-512x512.png'
 ];
 
 self.addEventListener('install', event => {
